69fc67631bd2f927b0d25913d48cfbe907763a8f
bcwzx <2237455121@qq.com>
2022-03-13 23:04:25 +0800

    Timecomplexity

---
+++ TimeComplexity/时间复杂度.cpp
@@ -0,0 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+
+
+
+
+
+
+
+
+
+//输入一个正数（按位与）
+//int count(BYTE n)
+//{
+//	int num = 0;
+//	while (n)
+//	{
+//		n = n & (n - 1);
+//		num++;
+//	}
+//	return num;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+//计算Bubblesort的时间复杂度
+//F（N）=O(N^2)
+// 函数：（N-1）*N/2
+//void Bubblesort（int*a, int n)
+
+
+
+
+
+
+
+
+//计算strchr时间复杂度
+//const char strchr(const char*str,int character )
+
+
+//while (*str != "\0")
+//{
+//	if (*str == character)
+//	
+//		return str;
+//	++str;
+//	
+// 最好O（1）  最坏O(N)  平均O(N/2)
+// 默认最坏 O（N）
+//}
+
